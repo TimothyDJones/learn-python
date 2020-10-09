3339457e9f38418916c9fd5a5d7cd3027b4a84de
Tim Jones <tdjones74021@yahoo.com>
2020-10-09 10:18:42 -0500

    Add human readable duration format kata from Codewars.

---
+++ codewars/human_readable_duration_format.py
@@ -0,0 +1,44 @@
+# human_readable_duration_format.py
+# https://www.codewars.com/kata/52742f58faf5485cae000b9a/train/python
+
+def format_unit(value, unit):
+    result = ""
+    if value:
+        result = str(value) + " " + unit
+        if value > 1:
+            result += "s"
+    return result
+
+def format_duration(seconds):
+    if seconds == 0:
+        return "now"
+    
+    time_dict = {"minute": 60, "hour": (60*60), "day": (24*60*60), "year": (365*24*60*60)}
+
+    years = seconds // time_dict["year"]
+    time_left = seconds % time_dict["year"]
+    days = time_left // time_dict["day"]
+    time_left = time_left % time_dict["day"]
+    hours = time_left // time_dict["hour"]
+    time_left = time_left % time_dict["hour"]
+    minutes = time_left // time_dict["minute"]
+    seconds = time_left % time_dict["minute"]
+    #print(years, days, hours, minutes, seconds)
+    
+    result = y = d = h = m = s = ""
+    if years: y = format_unit(years, "year")
+    if days: d = format_unit(days, "day")
+    if hours: h = format_unit(hours, "hour")
+    if minutes: m = format_unit(minutes, "minute")
+    if seconds: s = format_unit(seconds, "second")
+    #print(y, d, h, m, s)
+    result = ", ".join([i for i in [y, d, h, m, s] if i != ''])
+    if result.rfind(", ") > 0:
+        result = result[:result.rfind(", ")] + " and" + result[result.rfind(", ") + 1:]
+    #print(result)
+    return result
+    
+if __name__ == "__main__":
+    print(format_duration(1))
+    print(format_duration(60))
+    print(format_duration(3662))
